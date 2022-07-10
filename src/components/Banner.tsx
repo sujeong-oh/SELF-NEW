c4def169b8f644e1cc01e355afd1ba2ba1549e90
DESKTOP-08NVLIS\수정 <osj_054@naver.com>
2022-07-10 12:04:44 +0900

    no message

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
     <div className="bg-blue-300 mb-3">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8 ">
         <div className=" flex-1 flex items-center"> 달력 API/ 제목 
-        <div className="buttons absolute top-10 right-3">   {/* 오른쪽 끝에 위치시키고 싶은데 저걸 쓰는게 맞는지?*/}
+        <div className="buttons absolute top-10 right-3">   
           <button
             type="submit"
             className="-mr-1 p-2 rounded-md hover:bg-white 
